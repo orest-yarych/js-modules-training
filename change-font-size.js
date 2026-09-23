555efa2c48e003552c05d0c06ccd878b63918a96
orest-yarych <37873681+orest-yarych@users.noreply.github.com>
2026-09-23 11:05:54 +0000

    Deploying to gh-pages from @ orest-yarych/js-modules-training@083902eab9260c6aabf1e2fec6edf6f6d147abf8 🚀

---
+++ change-font-size.js
@@ -0,0 +1,2 @@
+import"./assets/styles-cif6foye.js";import{l as a,r as o,a as r,b as s,S as e}from"./assets/handlers-BSA7qDN_.js";const l=Number(a(e.FONT_SIZE))||18;o.readerTools.addEventListener("click",r);s(l);
+//# sourceMappingURL=change-font-size.js.map
---
+++ promo-window.js
@@ -0,0 +1,2 @@
+import"./assets/styles-cif6foye.js";import{l as r,r as o,o as s,S as e}from"./assets/handlers-BSA7qDN_.js";const i=r(e.PROMO_HIDDEN);i||(o.promoDiv.hidden=!1);o.closePromoBtn.addEventListener("click",s);
+//# sourceMappingURL=promo-window.js.map
